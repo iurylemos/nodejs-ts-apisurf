10c6f96d514b001980c4b2747103d5cd32b7674e
Iury Lemos <iurylemos10@gmail.com>
2020-12-26 14:09:52 -0300

    Mockando axios com o JEST e header fake em get

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -7,13 +7,21 @@ import stormGlassNormalize3Hours from '@test/fixtures/stormglass_normalized_resp
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+
+  //Dizendo pro typescript que o mockAxios é um axios por debaixo dos panos
+  //Pegando o tipo do axios, e vai adicionar os tipos do jest mock em cima do axios
+  //E assim estou criando o mock
+  const mockAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalize forecast from the StormGlass service', async () => {
     const lat = 33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3Hours });
+    // axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3Hours });
+    //Usando o mock fica assim
+    mockAxios.get.mockResolvedValue({ data: stormGlassWeather3Hours });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalize3Hours);
   });
